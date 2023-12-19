74e77225c2fcb5aa3622b58ce197c57f3ee09138
Steve Meyfroidt <steve.meyfroidt@gmail.com>
2023-12-19 11:42:13 +0000

    Format

--- neovim/.config/nvim/lua/plugins-enabled/mason-lspconfig.lua
+++ neovim/.config/nvim/lua/plugins-enabled/mason-lspconfig.lua
@@ -1,26 +1,26 @@
-local Plugin = {'williamboman/mason-lspconfig.nvim'}
+local Plugin = { "williamboman/mason-lspconfig.nvim" }
 
 Plugin.dependencies = {
-  {'williamboman/mason.nvim'},
+  { "williamboman/mason.nvim" },
 }
 
 function Plugin.config()
-  require("mason-lspconfig").setup {
-      ensure_installed = {
-        "bashls",
-        -- "cfn-lint",
-        "dockerls",
-        "jsonls",
-        "lua_ls",
-        -- "markdownlint",
-        -- "prettier",
-        -- "pylint",
-        "rubocop",
-        "solargraph",
-        "tsserver",
-        "yamlls",
-      },
-  }
+  require("mason-lspconfig").setup({
+    ensure_installed = {
+      "bashls",
+      -- "cfn-lint",
+      "dockerls",
+      "jsonls",
+      "lua_ls",
+      -- "markdownlint",
+      -- "prettier",
+      -- "pylint",
+      "rubocop",
+      "solargraph",
+      "tsserver",
+      "yamlls",
+    },
+  })
 end
 
 return Plugin
