4b09709ee86515c73b2b63d94422f318f1b5de4c
smeyfroi <steve.meyfroidt@gmail.com>
2025-04-03 17:35:08 +0100

    Improve lualine

--- neovim/.config/nvim/lua/plugins-enabled/lualine.lua
+++ neovim/.config/nvim/lua/plugins-enabled/lualine.lua
@@ -10,14 +10,38 @@ local Plugin = {
       disabled_filetypes = { statusline = { "snacks_dashboard" } },
     },
     sections = {
-      lualine_a = { "mode" },
+      -- lualine_a = {
+      --   {
+      --     'filetype',
+      --     colored = false,   -- Displays filetype icon in color if set to true
+      --     icon_only = true, -- Display only an icon for filetype
+      --     icon = { align = 'right' }, -- Display filetype icon on the right hand side
+      --   }
+      -- },
+      lualine_a = { { "mode", fmt = function(str) return str:sub(1,1) end } },
       lualine_b = { "branch" },
+      lualine_c = { { "filename", path = 1 } },
       lualine_x = {
         {
-          require("noice").api.statusline.mode.get,
-          cond = require("noice").api.statusline.mode.has,
+          require("noice").api.status.message.get_hl,
+          cond = require("noice").api.status.message.has,
+        },
+        {
+          require("noice").api.status.command.get,
+          cond = require("noice").api.status.command.has,
           color = { fg = "#ff9e64" },
-        }
+        },
+        {
+          require("noice").api.status.mode.get,
+          cond = require("noice").api.status.mode.has,
+          color = { fg = "#ff9e64" },
+        },
+        {
+          require("noice").api.status.search.get,
+          cond = require("noice").api.status.search.has,
+          color = { fg = "#ff9e64" },
+        },
+        { "diagnostics" },
       },
     },
   },
